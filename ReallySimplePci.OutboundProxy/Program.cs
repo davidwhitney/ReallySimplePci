f7bf73242749295be261b4483555a47ffab88bb1
David Whitney <david@davidwhitney.co.uk>
2013-08-07 22:03:42 +0100

    Added start of open pci reference implementation.

---
+++ ReallySimplePci.OutboundProxy/Program.cs
@@ -0,0 +1,13 @@
+﻿using ReallySimpleProxy;
+
+namespace ReallySimplePci.OutboundProxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var proxy = new ReallySimpleProxyHost(args, "ReallySimplePci.OutboundProxy");
+            proxy.Host("locahost", 12345);
+        }
+    }
+}
